c9f6d6776cf880c9c97666edd2044c55eab37eea
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-25 17:51:58 +0530

    Added and defined /profile and /logout routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const userService = require('../services/user.services');
 const { validationResult } = require('express-validator');
 
@@ -86,6 +87,8 @@ module.exports.loginUser = async (req, res, next) => {
 
     const token = user.generateAuthToken();
 
+    res.cookie('token', token);
+
     const { password: _, ...safeUser } = user.toObject();
 
     res.status(200).json({
@@ -106,3 +109,19 @@ module.exports.loginUser = async (req, res, next) => {
   }
 
 };
+
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token');
+
+  const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+  await blacklistTokenModel.create({ token });
+
+  res.status(200).json({
+    message: "Logged out!"
+  });
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
+const { authUser } = require('../middlewares/auth.middleware')
 
 router.post('/register', [
     body('fullname.firstname').isLength({ min: 2 }).withMessage('First name must be at leat 2 chars long'),
@@ -22,5 +23,9 @@ router.post('/login', [
 ],
     userController.loginUser
 )
+
+router.get('/profile', authUser, userController.getUserProfile);
+
+router.get('/logout', authUser, userController.logoutUser);
 
 module.exports = router;
