673cdfd19ea2467f2842e408be2e0e0622252f3a
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-25 14:24:47 +0530

    Minor Changes

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-async function connectToDB() {
+const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Connected to DB');
   } catch (err) {
     console.error('❌ DB connection failed:', err.message);
   }
 }
+
+module.exports = connectToDB;
