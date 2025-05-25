4b50471dd77dea3b7be76917214d05c4c4323e25
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-25 14:24:14 +0530

    Minor Changes

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes')
+
+connectToDB();
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/", (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/api/v1/user', userRoutes);
 
 module.exports = app;
