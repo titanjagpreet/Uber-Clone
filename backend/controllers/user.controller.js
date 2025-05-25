a993ab4251dbe13ccac3ef4dcdcd5f3119d90443
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-25 14:25:56 +0530

    Defined loginUser

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,7 +14,7 @@ module.exports.registerUser = async (req, res, next) => {
 
   try {
 
-    const { firstname, lastname, email, password } = req.body;
+    const { fullname, lastname, email, password } = req.body;
 
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
@@ -23,12 +23,12 @@ module.exports.registerUser = async (req, res, next) => {
         error: 'Email already in use',
       });
     }
-    
+
     const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
-      firstname,
-      lastname,
+      firstname: fullname.firstname,
+      lastname: fullname.lastname,
       email,
       password: hashedPassword,
     });
@@ -43,10 +43,66 @@ module.exports.registerUser = async (req, res, next) => {
         token,
         user: safeUser,
       },
+      message: "User registered successfully!"
     });
 
   } catch (err) {
 
-    next(err);
+    res.status(500).json({
+      error: err,
+      message: "Something went wrong during registration!"
+    })
   }
+};
+
+module.exports.loginUser = async (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        error: 'Invalid credentials. User not found.',
+      });
+    }
+
+    const matchPassword = await user.comparePassword(password); 
+    if (!matchPassword) {
+      return res.status(401).json({
+        success: false,
+        error: 'Invalid credentials. Password does not match.',
+      });
+    }
+
+    const token = user.generateAuthToken();
+
+    const { password: _, ...safeUser } = user.toObject();
+
+    res.status(200).json({
+      success: true,
+      data: {
+        token,
+        user: safeUser,
+      },
+      message: "User logged in successfully!"
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong during login!",
+    });
+  }
+
 };
