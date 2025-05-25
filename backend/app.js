45bc041e6855340190ab9101178631bacb7b80ac
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-25 17:49:37 +0530

    Minor changes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const connectToDB = require('./db/db');
 const userRoutes = require('./routes/user.routes')
@@ -10,6 +11,7 @@ connectToDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/user', userRoutes);
 
