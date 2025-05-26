8d98670208a7009e8924ea67240b8c7a19fbda49
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-26 19:45:33 +0530

    Imported and used Captain routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const connectToDB = require('./db/db');
-const userRoutes = require('./routes/user.routes')
+
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDB();
 
@@ -13,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/users', userRoutes);
+
+app.use('/api/v1/captains', captainRoutes);
 
 module.exports = app;
