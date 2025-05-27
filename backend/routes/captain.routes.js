1a6e7b386ddf2362f64d219200eb1dcb4d5c49e9
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-27 22:17:44 +0530

    Declared /login route with validation

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const captainController = require('../controllers/captain.controller');
 
 router.post('/register', [
 
@@ -21,6 +22,14 @@ router.post('/register', [
     body('vehicle.vehicleType').isIn(['car', 'motorcycle', 'auto']).withMessage('Invalid vehicle type')
 ],
     captainController.registerCaptain
+);
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 chars long')
+],
+    captainController.loginCaptain
 )
 
 module.exports = router;
