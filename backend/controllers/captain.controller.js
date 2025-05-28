b5379f0016ea1de3fe7ce09c027f873fc3e2a611
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-28 23:20:03 +0530

    Defined /profile and /logout routes of Captain

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,4 +1,5 @@
 const captainModel = require('../models/captain.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const captainService = require('../services/captain.services');
 const { validationResult } = require('express-validator');
 
@@ -116,3 +117,19 @@ module.exports.loginCaptain = async (req, res, next) => {
     }
 
 };
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+  res.status(200).json(req.captain);
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+  res.clearCookie('token');
+
+  const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+  await blacklistTokenModel.create({ token });
+
+  res.status(200).json({
+    message: "Logged out!"
+  });
+}
