1c0cc7ac18d31499c41277f38f44509701629cb4
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-07 16:32:28 +0530

    Wrraper to protect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx';
+import Start from './pages/Start.jsx';
 import UserSignup from './pages/UserSignup.jsx';
 import UserLogin from './pages/UserLogin.jsx';
 import CaptainSignup from './pages/CaptainSignup.jsx';
 import CaptainLogin from './pages/CaptainLogin.jsx';
+import Home from './pages/Home.jsx';
+import UserProtectWrapper from './pages/UserProtectWrapper.jsx'
 
 export default function App() {
 
   return (
     <>
     <Routes>
-      <Route path='/' element={ <Home/> } />
+      <Route path='/' element={ <Start/> } />
       <Route path='/signup' element={<UserSignup/>} />
       <Route path='/login' element={<UserLogin/>} />
       <Route path='/captain-signup' element={<CaptainSignup/>} />
       <Route path='/captain-login' element={<CaptainLogin/>} />
+
+      <Route path='/home' element={ 
+        <UserProtectWrapper>
+          <Home/>
+        </UserProtectWrapper>
+       } />
+
     </Routes>
     </>
   )
