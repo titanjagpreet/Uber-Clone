cc3f40aad7ac30d5ca09ea93a5f3d39bb296e847
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-08 23:17:05 +0530

    Create CaptainProtectWrapper and used it in captain routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,46 @@
 import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Start from './pages/Start.jsx';
 import UserSignup from './pages/UserSignup.jsx';
 import UserLogin from './pages/UserLogin.jsx';
 import CaptainSignup from './pages/CaptainSignup.jsx';
 import CaptainLogin from './pages/CaptainLogin.jsx';
 import Home from './pages/Home.jsx';
-import UserProtectWrapper from './pages/UserProtectWrapper.jsx'
+import UserProtectWrapper from './pages/UserProtectWrapper.jsx';
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper.jsx';
+import UserLogout from './pages/UserLogout.jsx';
+import CaptainHome from './pages/CaptainHome.jsx';
 
 export default function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={ <Start/> } />
-      <Route path='/signup' element={<UserSignup/>} />
-      <Route path='/login' element={<UserLogin/>} />
-      <Route path='/captain-signup' element={<CaptainSignup/>} />
-      <Route path='/captain-login' element={<CaptainLogin/>} />
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
 
-      <Route path='/home' element={ 
-        <UserProtectWrapper>
-          <Home/>
-        </UserProtectWrapper>
-       } />
+        <Route path='/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
 
-    </Routes>
+        <Route path='/user/logout' element={
+          <UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+        } />
+
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+        } />
+
+      </Routes>
     </>
   )
 }
