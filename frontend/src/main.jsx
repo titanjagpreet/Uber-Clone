031ce9f7d0071ee3c8460af6654c0c4fb212092e
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-08 23:15:22 +0530

    Created CaptainContext and wrapped App component with it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import UserContext from './context/UserContext.jsx';
+import CaptainContext from './context/CaptainContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserContext>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserContext>
+    <CaptainContext>
+      <UserContext>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContext>
+    </CaptainContext>
   </React.StrictMode>
 );
