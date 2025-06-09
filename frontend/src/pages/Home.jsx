2f4b442c0bd19ae809a42e31fcb843ee85007fe0
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-09 23:45:26 +0530

    Complete User Home Screen UI, used GSAP for panel logic and animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
 
 export default function Home() {
+
+  const [pickup, setPickup] = useState('');
+  const [destination, setDestination] = useState('');
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const arrowRef = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '72.5vh',
+        duration: 0.3,
+        ease: 'power2.out'
+      });
+      gsap.to(arrowRef.current, {
+        opacity: 1,
+        duration: 0.3,
+        ease: 'power2.out'
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0px',
+        duration: 0.3,
+        ease: 'power2.out'
+      });
+      gsap.to(arrowRef.current, {
+        opacity: 0,
+        duration: 0.3,
+        ease: 'power2.out'
+      });
+    }
+  }, [panelOpen]);
 
   return (
     <>
@@ -20,28 +59,54 @@ export default function Home() {
           />
         </div>
 
-        <div className='bg-white absolute bottom-0 w-full flex flex-col z-10 transition-all duration-300 ease-in-out h-auto'>
+        <div className='bg-white absolute bottom-0 w-full flex flex-col z-10 h-auto'>
 
-          <div className='!px-5 bg-white !py-4 flex-shrink-0'>
+          <div className='!px-5 bg-white !py-4 flex-shrink-0 relative'>
+
+            <h5 
+              ref={arrowRef}
+              className='absolute top-[10%] right-5 text-xl cursor-pointer'
+              onClick={() => {setPanelOpen(!panelOpen)}}
+              style={{ opacity: 0 }}
+            >
+              <i className="ri-arrow-down-wide-line"></i>
+            </h5>
 
             <h4 className='text-2xl font-semibold'>
               Find a trip
             </h4>
 
-            <form action="">
+            <form onSubmit={submitHandler}>
 
-              <div className='line absolute h-15 w-[2px] top-[44%] left-10 bg-gray-800 rounded-full'>
+              <div className='line absolute h-15 w-[2px] top-[44%] left-10 bg-gray-800 rounded-full'></div>
 
-              </div>
+              <input 
+                className='bg-[#eee] !px-10 !py-2 text-base rounded-lg w-full !mt-4' 
+                type="text" 
+                placeholder='Add a pickup'
+                value={pickup}
+                onChange={(e) => {setPickup(e.target.value)}} 
+                onClick={() => {setPanelOpen(true)}} 
+              />
 
-              <input className='bg-[#eee] !px-10 !py-2 text-base rounded-lg w-full !mt-4' type="text" placeholder='Add a pickup' />
-              <input className='bg-[#eee] !px-10 !py-2 text-base rounded-lg w-full !mb-3 !mt-3' type="text" placeholder='Enter destination' />
+              <input 
+                className='bg-[#eee] !px-10 !py-2 text-base rounded-lg w-full !mb-3 !mt-3' 
+                type="text" 
+                placeholder='Enter destination' 
+                value={destination}
+                onChange={(e) => {setDestination(e.target.value)}}
+                onClick={() => {setPanelOpen(true)}} 
+              />
 
             </form>
           </div>
 
-          <div className='bg-red-500 !p-5 hidden h-[70vh]'>
-
+          <div 
+            ref={panelRef} 
+            className={`${panelOpen ? 'bg-red-500 !p-5' : 'bg-transparent !p-0'} overflow-hidden transition-all duration-300`}
+            style={{ height: '0px' }}
+          >
+            {/* Panel content can go here */}
           </div>
         </div>
       </div>
