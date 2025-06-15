771fdabd8e8a40b169d3f67cff0398ac9f7099d4
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-15 23:44:20 +0530

    Made RidePopup

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 export default function CaptainHome() {
+
+  const ridePopupPanelRef = useRef(null);
+  const [ridePopupPanel, setRidePopupPanel] = useState(true);
+
+  useGSAP(() => {
+    if (ridePopupPanel) {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: 'translateY(0)'
+      })
+    }
+    else {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: 'translateY(100%)'
+      })
+    }
+  }, [ridePopupPanel]);
 
   return (
     <div className='h-screen'>
@@ -18,6 +37,10 @@ export default function CaptainHome() {
       </div>
       <div className='h-2/5 !p-6'>
         <CaptainDetails />
+      </div>
+
+      <div ref={ridePopupPanelRef} className='fixed z-10 bottom-0 bg-white !py-4 !px-3 w-full translate-y-full'>
+        <RidePopUp setRidePopupPanel={setRidePopupPanel} />
       </div>
 
     </div>
