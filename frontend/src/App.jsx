7b0df089d4603fdd48d27e327dd3597c0e7886db
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-17 23:16:53 +0530

    Added /captain-riding route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import CaptainProtectWrapper from './pages/CaptainProtectWrapper.jsx';
 import UserLogout from './pages/UserLogout.jsx';
 import CaptainHome from './pages/CaptainHome.jsx';
 import Riding from './pages/Riding.jsx';
+import CaptainRiding from './pages/CaptainRiding.jsx';
 
 export default function App() {
 
@@ -20,9 +21,11 @@ export default function App() {
         <Route path='/' element={<Start />} />
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/login' element={<UserLogin />} />
+
         <Route path='/captain-signup' element={<CaptainSignup />} />
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/riding' element={ <Riding/> } />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
 
         <Route path='/home' element={
           <UserProtectWrapper>
