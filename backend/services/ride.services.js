4f02e1b124e213fe6e865689a8e65c9da6263f5e
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-25 21:55:10 +0530

    Added function to get OTP

--- backend/services/ride.services.js
+++ backend/services/ride.services.js
@@ -35,3 +35,14 @@ async function getFare(pickup, destination) {
     
     return fare;
 }
+
+module.exports.getFare = getFare;
+
+function getOtp(num) {
+    function generateOtp(num) {
+        const otp = crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
+        return otp;
+    }
+    return generateOtp(num);
+}
+
